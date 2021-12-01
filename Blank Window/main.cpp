e5153e73c125e97b53d8f7ee75cfc7d90f7e2ea5
VaibhavSharma <68940014+LOSTCANTiM@users.noreply.github.com>
2021-12-01 10:50:55 +0530

    Update main.cpp

--- Blank Window/main.cpp
+++ Blank Window/main.cpp
@@ -1,1 +1,42 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+#include <iostream>
+
+const int WIDTH = 800;
+const int HEIGHT = 800;
+
+int main()
+{
+	if (!glfwInit)
+		return -1;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Learning", NULL, NULL);
+	if (!window)
+		return -1;
+
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	gladLoadGL();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.1f, 0.1f, 0.12f, 1.0f);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
